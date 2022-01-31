35c21ec74125088112d6b19e65742a2592933eca
Guilherme Augusto Rocha Sousa <guilhermesousadev@gmail.com>
2022-01-31 09:19:39 -0300

    Delete src directory

--- src/config/auth.ts
+++
@@ -1,4 +0,0 @@
-export default {
-    secret: process.env.APP_API_SECRET,
-    expire: '1d'
-}
--- src/shared/typeorm/connection.ts
+++
@@ -1,5 +0,0 @@
-import { createConnection } from 'typeorm';
-
-createConnection()
-    .then(() => console.log("Conectado com Sucesso"))
-    .catch(e => console.log(e))
